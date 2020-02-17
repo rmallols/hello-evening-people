c583e4b36aef2e4ae1a3401195ea80f53db59491
Ricardo Mallols <ricardo.mallols@gmail.com>
2020-02-17 19:01:56 +0000

    Updating the UI with some sexy boxes around :)

--- src/App.js
+++ src/App.js
@@ -5,20 +5,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello Evening people you are the best :)</h1>
+        <div>
+            <div className='container'>
+                <div className='item'>Monday</div>
+                <div className='item'>Tuesday</div>
+                <div className='item'>Wednesday</div>
+                <div className='item'>Thursday</div>
+                <div className='item'>Friday</div>
+            </div>
+        </div>
     </div>
   );
 }
